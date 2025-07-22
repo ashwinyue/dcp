99670e0792a677edfdeb03ed8c12343aaff309d1
mervyn <mervyn@hyx.com>
2025-07-22 23:09:05 +0800

    1

--- internal/nightwatch/watcher/all/all.go
+++ internal/nightwatch/watcher/all/all.go
@@ -5,4 +5,5 @@ import (
 	_ "github.com/ashwinyue/dcp/internal/nightwatch/watcher/cronjob/cronjob"
 	_ "github.com/ashwinyue/dcp/internal/nightwatch/watcher/cronjob/statesync"
 	_ "github.com/ashwinyue/dcp/internal/nightwatch/watcher/job/llmtrain"
+	_ "github.com/ashwinyue/dcp/internal/nightwatch/watcher/job/smsbatch"
 )
